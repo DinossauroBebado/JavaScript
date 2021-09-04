59f97d6eb93d47b6022abfcb35e5c945001a9b36
DinossauroBebado <guipsilva32@gmail.com>
2021-09-04 17:50:19 -0300

    statement

--- 01. Basics/basics.js
+++ 01. Basics/basics.js
@@ -5,3 +5,12 @@ document.getElementById("demo").innerHTML = 10 + 20
 document.write(20+20)
 
 window.alert(42)
+// ------------------------------------------------------------
+var x,y,z;
+
+x = 5;
+y = 6;
+z = x + y;
+
+document.getElementById("total").innerHTML = "the value of z is " + z + ".";
+// ------------------------------------------------------------
